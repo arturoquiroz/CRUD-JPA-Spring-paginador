62a034302961bd85386b079df6723a7d5e33da0a
arturoquiroz <37517709+arturoquiroz@users.noreply.github.com>
2018-06-23 22:30:16 -0500

    Add files via upload

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,27 @@
+/*Populate tables*/
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (1, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (2, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (3, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (4, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (5, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (6, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (7, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (8, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (9, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (10, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (11, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (12, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (13, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (14, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (15, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (16, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (17, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (18, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (19, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (20, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (21, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (22, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (23, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (24, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (25, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (26, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
